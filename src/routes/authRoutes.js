9d3d33d53b2d01610fde4afe6fece5a585b2c7bf
HUNTER\EMagallanes <elijah.magallanes@hunterindustries.com>
2018-11-01 17:04:30 -0700

    Started adding users to db

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,6 +7,26 @@ const authRouter = express.Router();
 function router() {
   authRouter.route('/signUp')
     .post((req, res) => {
+      const { username, password } = req.body;
+      const url = 'mongodb://localhost:27017';
+      const dbName = 'libraryApp';
+
+      (async function addUser() {
+        let client;
+        try {
+          client = await MongoClient.connect(url);
+          debug('Connected correctly to server');
+
+          const db = client.db(dbName);
+
+          const col = db.collection('users');
+          const user = { username, password };
+          const results = await col.insertOne(user);
+          debug(results);
+        } catch (err) {
+          debug(err.stack);
+        }
+      }());
       debug(req.body);
       req.login(req.body, () => {
         res.redirect('/auth/profile');
